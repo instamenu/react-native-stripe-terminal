d08aa18c9cdbcf34396b1c40afd60433050182f4
Shayan Guha <shayanguha@gmail.com>
2022-03-11 09:19:00 -0800

    Run linters

--- src/StripeTerminal.js
+++ src/StripeTerminal.js
@@ -1,66 +1,66 @@
-import { NativeEventEmitter, NativeModules } from "react-native";
-import EventEmitter from "events";
+import { NativeEventEmitter, NativeModules } from 'react-native';
+import EventEmitter from 'events';
 const { RNStripeTerminal } = NativeModules;
 const constants = RNStripeTerminal.getConstants();
 const nativeEventEmitter = new NativeEventEmitter(RNStripeTerminal);
 
 class StripeTerminal extends EventEmitter {
-  _connection = { status: "NOT_CONNECTED", readers: [] };
+  _connection = { status: 'NOT_CONNECTED', readers: [] };
   _payment = {};
   get connection() {
     return this._connection;
   }
   set connection(value) {
     this._connection =
-      typeof value === "function" ? value(this._connection) : value;
-    this.emit("connectionChange", this._connection);
+      typeof value === 'function' ? value(this._connection) : value;
+    this.emit('connectionChange', this._connection);
   }
   get payment() {
     return this._payment;
   }
   set payment(value) {
-    this._payment = typeof value === "function" ? value(this._payment) : value;
-    this.emit("paymentChange", this._payment);
+    this._payment = typeof value === 'function' ? value(this._payment) : value;
+    this.emit('paymentChange', this._payment);
   }
   initialize({ fetchConnectionToken }) {
     if (this.initialized) {
-      ("StripeTerminal.initialize(...) has already been called. Skipping.");
+      ('StripeTerminal.initialize(...) has already been called. Skipping.');
       return;
     }
     this.initialized = true;
-    nativeEventEmitter.addListener("requestConnectionToken", () =>
+    nativeEventEmitter.addListener('requestConnectionToken', () =>
       fetchConnectionToken()
         .then((token) => {
-          if (!token) throw "";
+          if (!token) throw '';
           RNStripeTerminal.setConnectionToken(token, null);
         })
         .catch(() =>
           RNStripeTerminal.setConnectionToken(
             null,
-            "Error in fetchConnectionToken."
+            'Error in fetchConnectionToken.'
           )
         )
     );
-    nativeEventEmitter.addListener("readersDiscovered", (readers) => {
+    nativeEventEmitter.addListener('readersDiscovered', (readers) => {
       this.connection = { ...this.connection, readers };
     });
-    nativeEventEmitter.addListener("readerDisconnectCompletion", () => {
+    nativeEventEmitter.addListener('readerDisconnectCompletion', () => {
       this.connection = {
         ...this.connection,
-        status: "NOT_CONNECTED",
+        status: 'NOT_CONNECTED',
         reader: undefined,
       };
     });
-    nativeEventEmitter.addListener("readerConnection", (reader) => {
+    nativeEventEmitter.addListener('readerConnection', (reader) => {
       this.connection = {
         ...this.connection,
-        status: "CONNECTED",
+        status: 'CONNECTED',
         reader,
         readers: [],
       };
     });
     nativeEventEmitter.addListener(
-      "didReportReaderSoftwareUpdateProgress",
+      'didReportReaderSoftwareUpdateProgress',
       (progress) => {
         this.connection = {
           ...this.connection,
@@ -68,41 +68,41 @@ class StripeTerminal extends EventEmitter {
         };
       }
     );
-    nativeEventEmitter.addListener("didFinishInstallingUpdate", () => {
+    nativeEventEmitter.addListener('didFinishInstallingUpdate', () => {
       this.connection = { ...this.connection, update: undefined };
     });
-    nativeEventEmitter.addListener("didStartInstallingUpdate", (update) => {
+    nativeEventEmitter.addListener('didStartInstallingUpdate', (update) => {
       this.connection = {
         ...this.connection,
         update: { ...update, progress: 0 },
       };
     });
     nativeEventEmitter.addListener(
-      "didRequestReaderDisplayMessage",
+      'didRequestReaderDisplayMessage',
       ({ text }) => {
-        console.log("didRequestReaderDisplayMessage", text);
+        console.log('didRequestReaderDisplayMessage', text);
         this.payment = { ...this.payment, displayMessage: text };
       }
     );
-    nativeEventEmitter.addListener("didRequestReaderInput", ({ text }) => {
-      console.log("didRequestReaderInput", text);
+    nativeEventEmitter.addListener('didRequestReaderInput', ({ text }) => {
+      console.log('didRequestReaderInput', text);
       this.payment = { ...this.payment, inputRequest: text };
     });
-    nativeEventEmitter.addListener("didChangePaymentStatus", (...args) => {
-      console.log("didChangePaymentStatus", args);
+    nativeEventEmitter.addListener('didChangePaymentStatus', (...args) => {
+      console.log('didChangePaymentStatus', args);
       // this.payment = { ...this.payment, inputRequest: text };
     });
     RNStripeTerminal.initialize();
   }
   discoverReaders(discoveryMethod, simulated) {
-    this.connection = { ...this.connection, status: "DISCOVERING" };
+    this.connection = { ...this.connection, status: 'DISCOVERING' };
     RNStripeTerminal.discoverReaders(discoveryMethod, simulated);
   }
   abortDiscoverReaders() {
     RNStripeTerminal.abortDiscoverReaders().then(() => {
       this.connection = {
         ...this.connection,
-        status: "NOT_CONNECTED",
+        status: 'NOT_CONNECTED',
         readers: [],
       };
     });
@@ -111,7 +111,7 @@ class StripeTerminal extends EventEmitter {
     return nativeEventEmitter.addListener(...args);
   }
   connectReader(serialNumber, locationId) {
-    this.connection = { ...this.connection, status: "CONNECTING" };
+    this.connection = { ...this.connection, status: 'CONNECTING' };
     RNStripeTerminal.connectReader(serialNumber, locationId);
   }
   disconnectReader() {
@@ -119,16 +119,16 @@ class StripeTerminal extends EventEmitter {
   }
   async createPaymentIntent(parameters) {
     if (!parameters?.amount) {
-      throw "You must provide an amount to createPaymentIntent.";
+      throw 'You must provide an amount to createPaymentIntent.';
     }
     if (!parameters?.currency) {
       console.warn(
-        "No currency provided to createPaymentIntent. Defaulting to `usd`."
+        'No currency provided to createPaymentIntent. Defaulting to `usd`.'
       );
     }
     const paymentIntent = await RNStripeTerminal.createPaymentIntent({
       amount: parameters.amount,
-      currency: parameters?.currency ?? "usd",
+      currency: parameters?.currency ?? 'usd',
     });
     this.payment = { paymentIntent };
     return paymentIntent;
@@ -136,13 +136,13 @@ class StripeTerminal extends EventEmitter {
 
   async collectPaymentMethod({ paymentIntent }) {
     if (!paymentIntent) {
-      throw "You must provide a paymentIntent to collectPaymentMethod.";
+      throw 'You must provide a paymentIntent to collectPaymentMethod.';
     }
-    this.payment = { ...this.payment, status: "COLLECTING_PAYMENT_METHOD" };
+    this.payment = { ...this.payment, status: 'COLLECTING_PAYMENT_METHOD' };
     const paymentMethod = await RNStripeTerminal.collectPaymentMethod(
       paymentIntent
     ).catch((e) => {
-      if (e.message === "The command was canceled.") {
+      if (e.message === 'The command was canceled.') {
         // if the command was manually canceled, don’t consider it an error
         return;
       }
@@ -154,15 +154,15 @@ class StripeTerminal extends EventEmitter {
 
   async abortCollectPaymentMethod() {
     return RNStripeTerminal.abortCollectPaymentMethod().then(() => {
-      this.payment = { ...this.payment, status: "" };
+      this.payment = { ...this.payment, status: '' };
     });
   }
 
   async processPayment({ paymentIntent }) {
     if (!paymentIntent) {
-      throw "You must provide a paymentIntent to processPayment.";
+      throw 'You must provide a paymentIntent to processPayment.';
     }
-    this.payment = { ...this.payment, status: "PROCESSING_PAYMENT" };
+    this.payment = { ...this.payment, status: 'PROCESSING_PAYMENT' };
     const payment = await RNStripeTerminal.processPayment(paymentIntent).catch(
       (e) => {
         // if (e.message === "The command was canceled.") {
